3defcb1d307b0bf620b8001ccf1a27960132051e
Isaac <isaac.brian21@outlook.com>
2026-06-01 16:50:03 -0300

    feature: implementa camada de persistência no código

---
+++ postal-code-consultation-api/src/main/java/com/postal_code_consultation_api/repository/ConsultationLogRepository.java
@@ -0,0 +1,9 @@
+package com.postal_code_consultation_api.repository;
+
+import com.postal_code_consultation_api.entity.ConsultationLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ConsultationLogRepository extends JpaRepository<ConsultationLog, Long> {
+}
