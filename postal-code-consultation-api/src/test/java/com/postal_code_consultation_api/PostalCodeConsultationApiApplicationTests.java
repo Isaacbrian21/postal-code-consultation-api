60b80393a96235461b0bad545e4f282052b96219
Isaac <isaac.brian21@outlook.com>
2026-06-01 15:36:26 -0300

    Primeiro commit

---
+++ postal-code-consultation-api/src/test/java/com/postal_code_consultation_api/PostalCodeConsultationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.postal_code_consultation_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostalCodeConsultationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
