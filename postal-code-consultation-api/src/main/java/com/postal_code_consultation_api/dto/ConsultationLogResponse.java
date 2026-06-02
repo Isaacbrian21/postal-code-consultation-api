cf86bb9fc973957325a8ec180983406370d65e8e
Isaac <isaac.brian21@outlook.com>
2026-06-02 20:25:48 -0300

    feature: adciona schema annotation para swagger

--- postal-code-consultation-api/src/main/java/com/postal_code_consultation_api/dto/ConsultationLogResponse.java
+++ postal-code-consultation-api/src/main/java/com/postal_code_consultation_api/dto/ConsultationLogResponse.java
@@ -1,12 +1,19 @@
 package com.postal_code_consultation_api.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.time.LocalDateTime;
 
 public record ConsultationLogResponse(
 
+        @Schema(example = "1")
         Long id,
+
+        @Schema(example = "01310100")
         String cep,
+
         LocalDateTime consultationTime,
+
         ViaCepResponse response
 
 ) {
