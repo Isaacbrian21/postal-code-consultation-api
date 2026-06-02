8a613e62ea46020d85dd9249e4dc4956a5e2b12d
Isaac <isaac.brian21@outlook.com>
2026-06-02 20:20:57 -0300

    feature: Remove imports não utilizados

--- postal-code-consultation-api/src/main/java/com/postal_code_consultation_api/controller/PostalCodeController.java
+++ postal-code-consultation-api/src/main/java/com/postal_code_consultation_api/controller/PostalCodeController.java
@@ -3,14 +3,12 @@ package com.postal_code_consultation_api.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.postal_code_consultation_api.dto.ViaCepResponse;
-import com.postal_code_consultation_api.entity.ConsultationLog;
-import com.postal_code_consultation_api.service.PostalCodeService;
-import lombok.RequiredArgsConstructor;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
 
 import java.util.List;
 
