5df6195ac52943363f77b2becfca7cd5c5d5d3ca
Isaac <isaac.brian21@outlook.com>
2026-06-02 20:24:25 -0300

    feature: Adciona instancia da variável do wiremock/ViaCep

--- postal-code-consultation-api/src/main/java/com/postal_code_consultation_api/client/ViaCepClient.java
+++ postal-code-consultation-api/src/main/java/com/postal_code_consultation_api/client/ViaCepClient.java
@@ -1,11 +1,16 @@
 package com.postal_code_consultation_api.client;
 
 import com.postal_code_consultation_api.dto.ViaCepResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 
 @Component
 public class ViaCepClient {
+
+    @Value("${external.viacep.url}")
+    private String baseUrl;
+
     private final RestClient restClient;
 
     public ViaCepClient(RestClient restClient) {
@@ -15,7 +20,7 @@ public class ViaCepClient {
     public ViaCepResponse search(String cep) {
 
         return restClient.get()
-                .uri("https://viacep.com.br/ws/{cep}/json/", cep)
+                .uri(baseUrl + "/{cep}/json", cep)
                 .retrieve()
                 .body(ViaCepResponse.class);
     }
